d3a269b7834e5f6bec3576e65f1b0b4380d6237f
mohan <mohankukreja1@gmail.com>
2020-01-15 13:41:11 +0530

    array

--- routes/index.js
+++ routes/index.js
@@ -148,11 +148,35 @@ router.post('/addHospital',function(req,res){
 router.post('/addDoctor',function(req,res){
   console.log(req);
   var newUser=new doctor();
+  var checkSlots = {
+    "8-9": ["8.00","8.15","8.30","8.45"],
+    "9-10": ["9.00","9.15","9.30","9.45"],
+    "10-11": ["10.00","10.15","10.30","10.45"],
+    "11-12": ["11.00","11.15","11.30","11.45"],
+    "12-13": ["12.00","12.15","12.30","12.45"],
+    "13-14": ["13.00","13.15","13.30","13.45"],
+    "14-15": ["14.00","14.15","14.30","14.45"],
+    "15-16": ["15.00","15.15","15.30","15.45"],
+    "16-17": ["16.00","16.15","16.30","16.45"],
+    "17-18": ["17.00","17.15","17.30","17.45"],
+    "18-19": ["18.00","18.15","18.30","18.45"],
+    "19-20": ["19.00","19.15","19.30","19.45"]
 
+  }
+  finalSlot= [];
+  var tempArr = req.body.slot;
+  for(var i=0;i<tempArr.length;i++){
+    var adding = checkSlots[tempArr[i]];
+    finalSlot.push(adding);
+    
+  }
+  
+ 
   newUser.Name  =  req.body.dname;
   newUser.Hospital = req.body.hospital;
   newUser.Degree = req.body.degree;
-  newUser.Slots = req.body.slot;
+
+  newUser.Slots = finalSlot;
   newUser.Specialization = req.body.specialization;
   newUser.save(function (err) {
       if(err) throw (err);
